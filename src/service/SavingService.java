16748293dc55caac4c0979b7959bbb632147c2b9
tuan-binh <binh1892001@gmail.com>
2023-07-04 17:24:26 +0700

    upload

--- src/service/SavingService.java
+++ src/service/SavingService.java
@@ -1,16 +1,26 @@
 package service;
 
+import config.Message;
 import model.Saving;
 
 import java.util.List;
 
 public class SavingService {
 	private void save(List<Saving> list, Saving element) {
-	
+		if (findById(list, element.getId()) == null) {
+			list.add(element);
+		} else {
+			list.set(list.indexOf(findById(list, element.getId())), element);
+		}
 	}
 	
 	public void delete(List<Saving> list, int id) {
-	
+		if (findById(list, id) == null) {
+			System.err.println(Message.NOT_FOUND);
+			return;
+		}
+		list.remove(findById(list, id));
+		System.out.println(Message.DELETE_SUCCESS);
 	}
 	
 	public Saving findById(List<Saving> list, int id) {
@@ -22,5 +32,13 @@ public class SavingService {
 		return null;
 	}
 	
-	
+	public int getNewId(List<Saving> list) {
+		int idMax = 0;
+		for (Saving s : list) {
+			if (s.getId() > idMax) {
+				idMax = s.getId();
+			}
+		}
+		return idMax + 1;
+	}
 }
